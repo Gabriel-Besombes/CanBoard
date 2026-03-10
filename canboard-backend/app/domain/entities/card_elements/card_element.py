cacfaf8f679031f37e8b024bc496ad8dca85dd2c
Gabriel Besombes <gabybesombes84@gmail.com>
2026-03-10 16:53:51 +0100

    Update car elements with new structure

--- canboard-backend/app/domain/entities/card_elements/card_element.py
+++ canboard-backend/app/domain/entities/card_elements/card_element.py
@@ -1,37 +1,9 @@
-from app.domain.base_entity import BaseEntity
+from app.domain.entities.base_entity import BaseEntity
 from app.domain.values.name import Name
-from app.domain.values.entity_id import EntityId
-from abc import ABC
+from typing import Any
 
-class CardElement(BaseEntity, ABC):
+class CardElement(BaseEntity):
     """Domain entity representing a card element."""
     
-    def __init__(self, name: Name, content: object, id: EntityId | None = None):
-        """
-        Initialize a CardElement.
-        
-        Args:
-            id: The unique identifier for the card element.
-            name: The name of the card element.
-            content: The content of the card element, can be of any type depending on the specific element.
-        """
-        super().__init__(id=id)
-        self._name = name
-        self._content = content
-        
-    @property
-    def name(self) -> Name:
-        return self._name
-    
-    @name.setter
-    def name(self, new_name: Name):
-        if not isinstance(new_name, Name):
-            raise ValueError("Name must be a Name instance")
-        self._name = new_name
-
-    @property
-    def content(self):
-        return self._content
-
-    def __repr__(self):
-        return f"CardElement(id={self.id}, name={self.name}, content={self.content})"
+    name: Name
+    content: Any
--- canboard-backend/app/domain/entities/card_elements/text_field.py
+++ canboard-backend/app/domain/entities/card_elements/text_field.py
@@ -1,21 +1,6 @@
-from app.domain.card_elements.card_element import CardElement
-from app.domain.values.name import Name
-from app.domain.values.entity_id import EntityId
+from app.domain.entities.card_elements.card_element import CardElement
 
 class TextField(CardElement):
     """Domain entity representing a text field card element."""
     
-    def __init__(self, name: Name, content: str, id: EntityId | None = None):
-        """
-        Initialize a TextField.
-        
-        Args:
-            id: The unique identifier for the card element.
-            name: The name of the card element.
-            content: The text content of the text field.
-        """
-        super().__init__(id=id, name=name, content=content)
-
-    @property
-    def content(self) -> str:
-        return self._content
+    content: str
